f237098b585451568343893f9a22142ccde394ab
KitkouskiKalani-FS-1 <KMKitkouski@student.fullsail.edu>
2023-04-30 20:32:06 -0500

    created items page to be as it would be on release

---
+++ dev/Stats_Project/client/src/components/ItemCard.js
@@ -0,0 +1,17 @@
+import Card from 'react-bootstrap/Card';
+import ItemIcon from "../images/Jaksho_Icon.jpeg";
+
+function ChampionCard() {
+    return (
+        <Card className="text-center" style={{ width:'10rem', backgroundColor:"#404EED", padding:"0,10px,10px,  10px"}}>
+          <Card.Body style={{backgroundColor:"#404EED"}}>
+            <Card.Text style={{color:"white", margin:"auto"}}>
+              Jaksho
+            </Card.Text>
+          </Card.Body>
+          <Card.Img variant="bottom" width="80" height="140" src={ItemIcon} />
+        </Card>
+    );
+  }
+  
+  export default ChampionCard;
